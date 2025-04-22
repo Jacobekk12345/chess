20198100f8a3bed9aa478b2d1b121ae8b42e56d4
Jacobekk12345 <123406995+Jacobekk12345@users.noreply.github.com>
2025-04-22 17:14:09 +0200

    ChessPiece class

---
+++ ChessPiece.cpp
@@ -0,0 +1,49 @@
+#include "ChessPiece.h"
+
+ChessPiece::ChessPiece(PieceColor color, PieceType type, sf::Vector2i position) : color(color), type(type), boardPosition(position) {
+    // Create the texture as a member variable
+    loadTexture(makeFilePath(this->color, this->type));
+}
+ChessPiece::ChessPiece() {
+    this->blank = true;
+}
+bool ChessPiece::isBlank() const {
+    return this->blank;
+}
+void ChessPiece::loadTexture(std::string imagePath) {
+    // Load directly into the member texture
+    this->texture.loadFromFile(imagePath);
+    this->texture.setSmooth(true);
+}
+
+std::string makeFilePath(PieceColor color, PieceType type) {
+    std::string path;
+    path += "assets/pieces/";
+    path += std::to_string(static_cast<int>(color));
+    path += std::to_string(static_cast<int>(type));
+    path += ".png";
+    return path;
+}
+
+void ChessPiece::place(sf::RenderWindow& window) {
+
+    sf::Sprite sprite(texture);
+    sprite.setPosition(sf::Vector2f(boardPosition.x * 100, boardPosition.y * 100));
+    window.draw(sprite);
+}
+
+sf::Vector2i ChessPiece::getPosition() const {
+    return this->boardPosition;
+}
+
+void ChessPiece::setPosition(sf::Vector2i newPos) {
+    this->boardPosition = newPos;
+}
+
+PieceType ChessPiece::getType() const {
+    return this->type;
+}
+
+PieceColor ChessPiece::getColor() const {
+    return this->color;
+}
---
+++ ChessPiece.h
@@ -0,0 +1,42 @@
+#ifndef PIECE_H
+#define PIECE_H
+
+#include <SFML/Graphics.hpp>
+
+enum class PieceColor {
+	WHITE = 0, BLACK
+};
+
+enum class PieceType {
+	PAWN = 0, KNIGHT, BISHOP, ROOK, QUEEN, KING
+};
+
+class ChessPiece {
+private:
+
+	sf::Texture texture;
+	sf::Vector2i boardPosition;
+	PieceColor color;
+	PieceType type;
+	std::vector<sf::Vector2i> possibleMoves;
+
+	void loadTexture(std::string imagePath);
+	bool blank = false;
+public:
+	bool isSelected = false;
+
+	ChessPiece(PieceColor color, PieceType type, sf::Vector2i position);
+	ChessPiece();
+
+	void place(sf::RenderWindow& window);
+
+	sf::Vector2i getPosition() const;
+	void setPosition(sf::Vector2i newPos);
+	PieceType getType() const;
+	PieceColor getColor() const;
+	bool isBlank() const;
+};
+
+std::string makeFilePath(PieceColor color, PieceType type);
+
+#endif PIECE_H
