c205c649f0a274a95cfd816b1e4a2ad7df8061ae
Jacobekk12345 <123406995+Jacobekk12345@users.noreply.github.com>
2025-04-25 23:58:01 +0200

    main.cpp
    moved the main game loop logic to Setup

--- main.cpp
+++ main.cpp
@@ -1,15 +1,8 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/Window/Mouse.hpp>
-#include <SFML/Window/Event.hpp>
-
-#include <array>
-#include <iostream>
-
 #include "Setup.h"
 
 int main() {
-    
     Setup setup;
     setup.windowLoop();
 
+    return 0;
 }
